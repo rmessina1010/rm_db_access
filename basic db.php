6cf9461e0473f8471beb5d960098e5148e63a1f3
Ray Messina <rmessina10@gmail.com>
2021-04-22 18:31:39 -0500

    Fleshed out CRUD qruery writing functions

--- basic db.php
+++ basic db.php
@@ -1,7 +1,7 @@
 <?
 
 class DB{
-
+	
 	protected $user 	= null;
 	protected $password	= null;
 	protected $db_name 	= null;
@@ -19,16 +19,16 @@ class DB{
 		$this->db_host 	= $db_host;
 		$this->setup();
 	}
-
+	
 	protected function setup(){
 		$this->dbhs = array();
 		$this->db_dsn = $this->db_type.":dbname=".$this->db_name.";host=".$this->db_host;
 	}
-
+	
 	function connect($dbh='_default'){
  		return $this->open($dbh) ? $this->dbhs[$dbh] : null;
 	}
-
+	
 	function open($dbh='_default'){
 		if (is_scalar($dbh)){
 			if (!isset($this->dbhs[$dbh])){
@@ -44,7 +44,7 @@ class DB{
 	}
 
 	function close($dbh='_default'){
-		if (is_scalar($dbh)){
+		if (is_scalar($dbh)){ 
 			unset ($this->dbhs[$dbh]);
 		}
 		return $this;
@@ -54,7 +54,7 @@ class DB{
 
 
 class DB_query{
-
+	
 	protected $dbh 		= null;
 	protected $query	= 	'';
 	protected $STMNT	= null;
@@ -64,31 +64,31 @@ class DB_query{
 	protected $hold_ct	= 0;
 	protected $params	= null;
 
-
+	
 	function __construct($query,$dbh, array $attributes = array()){
  	  $this->dbh =  $dbh;
  	  $this->prep($query,$attributes);
 	  return $this;
 	}
-
+	
 	function prep($query, array $attributes = array()){
 		$this->hold_ct 	= preg_match_all("/:\w+/", $query, $matches);
  		$this->is_posit	= !($this->hold_ct > 0 );
  		$holders		= ($matches && $matches[0]) ?  $matches[0] : array();
- 		if ($this->is_posit) {
+ 		if ($this->is_posit) { 
 	 		$query = str_replace(':?', '?', $query,$count);
 	 		$this->hold_ct =$count;
-	 		$this->holders= $count >0 ? array_fill(1, $count, true) : array();
+	 		$this->holders= $count >0 ? array_fill(1, $count, true) : array(); 
 	 	}else{
 	 		$this->holders	= ($matches && $matches[0]) ?  array_flip($matches[0]) : array();
 	 	}
 		$this->query  	= $query;
  		$this->STMNT  = $this->dbh->prepare($query,$attributes);
-
+		
 		return $this;
 	}
-
-
+	
+ 	
 	function bind_param(&$var,$col,$type='STR'){
 		if ($this->STMNT){
 			$col  =  $this->col_check($col);
@@ -99,7 +99,7 @@ class DB_query{
 		}
 		return $this;
 	}
-
+	
 	function bind_val($val,$col,$type='STR'){
 	 	if ($this->STMNT){
 			$col  =  $this->col_check($col);
@@ -110,28 +110,28 @@ class DB_query{
 		}
 		return $this;
 	}
-
+	
 	function list_holders(){
 		return array_keys($this->holders);
 	}
-
+	
 	function has_holder($key){
 		return isset($this->holders[$key]);
 	}
-
+	
 	function is_positional(){
 		return $this->is_posit;
 	}
-
+	 	
 	function set_params(array $params =array(),$type ='PARAM_STR'){////complete later
 		foreach ($params as $col=>$val ) {
 			$this->bindVal();
 		}
 		return $this;
 	}
-
+	
 	function STMNT(){ return $this->STMNT;}
-
+	
 	function run($data = null){
 		if ($this->STMNT){
 			if (is_array($data)){    $this->STMNT->execute($data) ;}
@@ -139,8 +139,8 @@ class DB_query{
 		}
 		return $this->STMNT;
 	}
-
-
+	
+	
 	protected function col_check($col){
 		if (is_string($col)) { $col = trim($col);}
  		if (!$this->is_posit){
@@ -154,7 +154,7 @@ class DB_query{
 		}
 		return $col;
 	}
-
+	
 	protected function type_check($type){
 		if (!is_string($type) || !preg_match('/^ *(?:BOOL|NULL|INT|STR|LOB|STMT) *$/i', $type)){ $type = 'STR'; }
 		$type = 'PARAM_'.strtoupper(trim($type));
@@ -163,74 +163,118 @@ class DB_query{
 }
 
 
-function sql_str($operation, $table, $cols,$vals){
+function sql_str($operation, $table,  array $args= array()){
 	$operation = is_scalar($operation) ? strtolower(trim($operation.'')) : 'r'; //sanitizes variable and/or defaults to 'r'
 	$operation = $operation  ? $operation[0] : 'r'; //sanitizes variable and/or defaults to 'r'
-	if (is_array($table)){
-
-		 $key_map = array( 't'=>'table', 'c'=>'cols', 'v'=>'vals', 's'=>'set_list', 'l'=>'limit', 'o'=>'order','w'=>'where', 'j'=>'join',    'g'=>'group', 'h'=>'having');
-
-
-	}
+		 
+	$key_map = array(  'c'=>'cols', 'v'=>'vals', 'l'=>'limit', 'o'=>'order','w'=>'where', 'g'=>'group', 'h'=>'having', 'sl'=>'set_list');
+	foreach ($key_map  as $mkey=>$mvar){ $$mvar = isset($args[$mkey])  ?  $args[$mkey] : null; 	}
+	
+	$join_key_map = array('j'=>'join',    'lj'=>'join','rj'=>'join','ij'=>'join');
+	foreach ($join_key_map  as $join_key=>$jval){ $joins[$join_key] = isset($args[$join_key]) ? $args[$join_key] : array(); }
+	 
 	$WHERE    = $where  ? ' WHERE '.$where : '';
-	$WHERE    = $having ? ' HAVING '.$having : '';
+	$HAVING   = $having ? ' HAVING '.$having : '';
 	$LIMIT    = $limit  ? ' LIMIT '.$limit : '';
 	$ORDERBY  = $order  ? ' ORDER BY '.$order : '';
 	$GROUPBY  = $group  ? ' GROUP BY '.$group : '';
-	//joins
-
+	$JOIN 	  = '';
+	$join_sequences = array (  'j'=>'', 'ij'=>' INNER JOIN ', 'lj'=>' LEFT JOIN ','rj'=>' RIGHT JOIN ' );
+	foreach( $join_sequences as $sequence =>$join_command ){
+		if (is_string($joins[$sequence])){
+			if ($joins[$sequence]) { $JOIN .= ($JOIN ? ' , ' : '').$join_command.$joins[$sequence]; }
+		}else{
+			foreach ($joins[$sequence] as $new_join){
+				if (is_string($new_join) && $new_join){ 
+					$JOIN .= ($JOIN ? ' , ' : '').$join_command.$joins[$sequence];
+				}
+			}
+		}
+ 	}
+	
+  	
  	switch ($operation){
-
 		case 'p':
 			$sql = $table;
 		break;
-
+		
 		case 'c':
-			$sql = "INSERT INTO $table SET ($cols) VALUES $values";
+	
+			if( is_array($cols)){
+				if (!$vals){
+					$vals = array_values($cols);
+					$cols =array_keys($cols);
+				}
+				$cols = implode(' , ', $cols);
+			}
+			if(is_array($vals)){
+				$val_str=array();
+				foreach($vals as $val){
+					if (!is_array($val)) { 
+						$val_str = implode(' , ', $vals); 
+						break;
+					}
+					$val_str[] =  implode(' , ', $val);
+				}
+				if (is_array($val_str)) {
+					$val_str = implode(') , (' , $val_str);
+				}
+				$vals = $val_str;
+			}
+			
+			$vals =  '('.$vals.')';
+			$sql = "INSERT INTO $table SET ($cols) VALUES $vals";
  		break;
-
+		
 		case 'u':
 			// set-list
 			$set_list_str = $comma ='';
-			if (!is_string($set_list)){// the set_list has been pre parsed
-				if(is_array($set_list)  && $set_list){ // the set_list has been passed as akey value pair array
-					$vals  = array_values($set_list);
-					$cols  = array_keys($set_list);
-				}elseif ( is_array($cols))   {
-					if ( is_scalar($vals)  ){
-						 $vals  = $vals  ?  array_values($cols) : array_fill(0, count($sols),':?');
-					}else{
-						$cols= array_values($col);
+			if (is_string($set_list)){ $set_list_str = $set_list;}
+			else{// the set_list has been pre parsed 
+				if(is_array($set_list)){ $cols  = $set_list;}
+				elseif ( is_array($cols)){ 
+					if ( !is_array($vals)  ){
+						$cols =  $vals ? array_combine($cols,array_fill(0, count($cols),':?')) : $cols;
 					}
-				}
-				foreach ($cols as $i=>$col){ $set_list_str .=  $comma.$col.' = '.$values[$i]; $comma =' , ';}
-			}else{ $set_list_str = $set_list;}
-			$sql = "UPDATE $table SET $set_list_str $WHERE $ORDERBY $HAVING $LIMIT";
+					else{ $cols =  array_combine($cols,$vals);}
+				} 
+				foreach ($cols as $col=>$val){ $set_list_str .=  $comma.$col.' = '.$val; $comma =' , ';}
+			}
+			$sql = "UPDATE $table $JOIN SET $set_list_str $WHERE $ORDERBY $HAVING $LIMIT";
 		break;
-
+		
 		case 'd':
 			$sql= "DELETE FROM $table $WHERE $ORDERBY $LIMIT";
 		break;
 		default:
+		    if( is_array($cols)){ 
+			    $cols= $vals ? implode(',', array_keys($cols)) : implode(',' , $cols )  ;
+			}
 			$sql = "SELECT $cols FROM $table $JOIN $WHERE $ORDERBY  $GROUPBY $HAVING $LIMIT";
-
+		
  	}
-
+ 	
  	return $sql;
  }
 
-function sql_C_str($table, $cols,$vals){
-
+function sql_C_str($table, $cols,$vals=null){
+	return sql_str('c',$table,array ('c'=>$cols, 'v'=>$vals));
 }
-function sql_R_str($table, $cols,$vals){////
-
+function sql_R_str($table, $cols='*',$where=null,$ord=null,$lim=null,array $args=array()){////
+	$args['w']=$where;
+	$args['c']=$cols;
+	$args['o']=$ord;
+	$args['l']=$lim;
+	return sql_str('r',$table,$args);
 }
-function sql_U_str($table, $cols,$vals){///
-
+function sql_U_str($table, $cols, $where, array $args=array()){///
+	$args['w']=$where;
+	$args['c']=$cols;
+	return sql_str('c',$table,$args);
 }
-function sql_D_str($table, $where,$ord, $lim){
-
+function sql_D_str($table, $where, $lim=null, $ord=null){
+	return sql_str('d',$table, array('w'=>$where, 'l'=>$lim, 'o'=>$ord));
 }
 function sql_p_str($table){
-
+	return sql_str('p',$table);
 }
