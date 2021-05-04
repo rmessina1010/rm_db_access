d23ad93d86df8ebbd44fe419ac544eea19dbf0ef
Ray Messina <rmessina10@gmail.com>
2021-05-03 21:09:38 -0500

    Adds dataDump() method

--- RMLDO.php
+++ RMLDO.php
@@ -175,7 +175,10 @@ class RMLDO{
      	if (isset($this->_TheData[$row])){return $this->_TheData[$row];}
  	}
  	
-	function dump(){
+	function dump($i=null, $l = null){
+		if ($i !== null ){
+			return array_slice($this->_TheData, $i, $l);
+		}
 		return $this->_TheData;
  	}
 
@@ -683,19 +686,4 @@ class RMLDO{
  		return  $SQL;
 	}	
  }
- 
- include ('basic db-4.php');  ECHO 'MMMMM!!!<HR>';
- $test = new RMLDO( 'SELECT * FROM Categories WHERE CatPub =? and Parent = :?',array ('vars'=>array(1,0)));
- $datax[] =array('x'=>1,'y'=>true,'z'=>'some string', 'a'=>array(1,2,3));
- $datax[] =array('x'=>2,'y'=>false,'z'=>'s string', 'a'=>array(10,5,3));
- $datax[] =array('x'=>3,'y'=>true,'z'=>'some string', 'a'=>array(11,2,33));
- $datax[] =array('x'=>4,'y'=>false,'z'=>'some string', 'a'=>array(1,2,3));
- $datax[] =array('x'=>5,'y'=>true,'z'=>'  string', 'a'=>array(1,2,3));
- $test2 = new RMLDO( $datax);
-  var_dump($test->thisRow(), $test->thisRow());
-  echo $test2->the_('z','<b>','</b>',array( 'offs'=>-1,'loop'=>true));
-  
-  //added deafult connection object
-  //fixed errorCode === '' value (5b)
-  // moved settings
 ?>
