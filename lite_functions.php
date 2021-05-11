5c9745a55b9d9216a385c8c961e5e94d9839a6b2
Ray Messina <rmessina10@gmail.com>
2021-05-10 23:07:53 -0500

    Added scrubbit and data sanitation fuctions  to fuction files

--- lite_functions.php
+++ lite_functions.php
@@ -35,3 +35,81 @@
 		  }
 		  return $str;
  	}
+ 
+    function rm_vsprintf_assoc($string, $array){
+	   if (!is_scalar($string)) { return  $string;  }
+	   if (is_object($array)) { $data = get_object_vars($data);  }
+	   if (!is_array($array)) { return  $string;  }
+	   preg_match_all('#%([\w\d-_]+@[^\\$]*)\\$#', $string,$dates);
+	   if (isset($dates[1])){$dates = array_unique($dates[1]);}
+	   foreach($dates as $date){
+		   $split = strpos($date, '@');
+		   $pattern = substr($date, $split+1);
+ 		   $key = substr($date, 0, $split );
+ 		   $string =str_replace("%$date$", date($pattern, $array[$key] ), $string);
+ 	   }
+	   $data = array();
+	   $count= 1;
+	   foreach ($array  as $key=> $val){
+		   $data[$count] =$val;
+		   $string =str_replace("%$key\$", "%$count\$", $string);
+		   $count++;
+	   }
+	   return vsprintf($string, $data);
+	}
+	
+function rm_scrub_empty_tags($html,$args =false){
+    $nbsp  = !isset($args['nbsp']) || $args['nbsp'] ? '\s*(?:&nbsp;\s*)*':'\s*' ;
+    $attrs = isset($args['attr']) && $args['attr'] ?    '' : '[^>]*' ;
+    $regex = '#<(\w[\w\d]*)\s*'.$attrs.'>'.$nbsp.'</\1>#i';
+  	$htm = preg_filter( $regex, '', $html);
+  	$html = $htm ? : $html;
+	return $html;
+}
+
+function rm_scrub_tags($html,$tag='style|script'){
+    $regex = '#<('.$tag.')[^>]*>.*</\1>#i';
+  	$htm  = preg_filter( $regex, '', $html);
+  	$html = $htm ? :$html;
+ 	return $html;
+}
+
+function rm_sani_attrs($html,array $allow =array()){
+	
+	$opts = array (
+			'css'=> array (  'class'=>1 ,'id'=>1, 'style'=>1),
+			'mouse'=> array ( 'up'=>1 ,'down'=>1, 'enter'=>1, 'leave'=>1, 'over'=>1, 'out'=>1),
+			'key'=> array ( 'up'=>1 ,'down'=>1, 'press'=>1),
+			'gen'=> array ( 'blur'=>1 ,'focus'=>1, 'change'=>1, 'load'=>1, 'click'=>1)
+	);
+	foreach ($allow as $group=>$type){
+		if (!is_array($type)){ continue;}
+ 		foreach ($type as $attr){
+ 			unset($opts[$group][$attr]);
+		}
+	}
+	foreach ($opts as $opt_key=>$opt){
+		$opts[$opt_key] =  $opts[$opt_key] ? '(?:'.implode('|',array_keys($opts[$opt_key])).')' :'';
+	}
+ 	$on=$close_on='';
+	if(($opts['mouse']||$opts['key']||$opts['gen'])){
+		if ($opts['css'] ) { $opts['css'].='|';}
+		$on='(?:on';
+ 		$close_on=')';
+ 		if ($opts['gen']){
+	 		$on.=substr($opts['gen'],0, -1).(($opts['mouse']||$opts['key']) ? '|':'');
+	 		$close_on.=')';
+ 		}
+ 		if ($opts['key']){
+	 		$on.='(?:key'.$opts['key'].')'.($opts['mouse'] ? '|':'');
+	 		$close_on.=')';
+ 		}
+ 		if ($opts['mouse']){
+	 		$on.='(?:mouse'.$opts['mouse'].')';
+  		}
+	}
+    $regex = '#(?:'.$opts['css'].$on.$close_on.'\s*=\s*([\'"]).*(?<!\\\\)\1#iU';
+   	$htm  = preg_filter( $regex, '', $html);
+  	$html = $htm ? :$html;
+ 	return $html;
+}
