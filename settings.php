9b24acee23a00ba5c16731464c643a95e0cfa5ff
Ray Messina <rmessina10@gmail.com>
2021-05-15 23:25:50 -0500

    Renamed 'basic db.php' to 'RMLDB.php' for consistency; consolidated basic settings in settings.php

--- settings.php
+++ settings.php
@@ -1,1 +1,13 @@
+<?
+define('DB_TYP_DEF','mysql');		//Default DB type.. new as of 5.0
+define('SCHEME', isset($_SERVER['HTTPS']) ? 'https://' : 'http://');
+define('DBNAME','dbname');
+define('HOST','localhost');
+define('UPASS','userpassword');
+define('UACC','username');
+
 define ('APPLY_SC', false);
+define('ADMIN_MAIL','email@email.com');// Your Email address
+define('ERR_404','_404.php');// It's handy to define ERRs as Constants
+define('T_OFFSET',MERID*3600);
+?>
