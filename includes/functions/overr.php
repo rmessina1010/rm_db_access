8285dff88fa068d4634be59d116f0b65f1f131f4
rmessina1010 <rmessina10@gmail.com>
2026-07-21 10:06:07 -0500

    draft of overload helper

---
+++ includes/functions/overr.php
@@ -0,0 +1,58 @@
+<?
+ // This is a utility to fascilitate function overloading in PHP
+ // Best used in combination with func_get_args()
+ 
+ function _overload( array $args, array $matchers, bool $return_all= false) {
+
+    $return_args = [];
+    $arg_ct =  count($arg);
+	$matched = false;
+	$arg_ty= implode(",", array_map('typeof', $args));
+	
+	// get types
+	// get vals
+	
+	foreach ($matchers as $match => $conditions){
+		$types = is_string($conditions) ? $conditions : false;
+		$values = false;
+		$count = 0;
+		if (isset($conditions['ty'])){ $types = $conditions['ty']; }
+		if (isset($conditions['vl'])){ $values = $conditions['vl'];}
+		$types = preg_replace('/\s+/', '', $types);
+		if (strlen($types)>2){ $count = 1 + substr_count(',', $types); }
+		// check  all types match in order
+		if ($count != $arg_ct) {continue;}
+		if ($types &&  $types != $arg_ty) {continue;}
+		// check vals
+		if ($values){
+			$k = 0;
+			foreach ($values as $v){
+				if ((is_array($v) && !in_array($args[$k], $v)) || $args[$k] !== $v){ continue 2;}
+				$k++;
+			}
+		}
+		$matched =$match;
+	}
+	
+	if ($matched && !$return_all){
+		if (is_array($return_all)){
+			$k =0 ;
+			foreach ($return_all as $k){
+				$return_args[$k]= $args[$k];
+				$k++;
+			}
+		}else{
+			$return_args= array_slice($args, 0, $arg_ct);
+		}
+	}
+
+	return [
+		'flag' => $matched,
+		'args' => $return_args,
+		];
+
+}
+
+  _overload( array(false,"SQL GoES HERE", [1,2,3,4], array ("sql"=>array("ty"=>"boolean,string,array")))
+
+?>
